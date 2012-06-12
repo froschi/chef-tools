c110ad670e47b0981b6c1e957b8e2c4f56e984fc
Thorsten Fischer <thorsten@froschi.org>
2012-06-12 14:43:02 +0200

    Add rudimentary readme-skel.sh.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

---
+++ readme-skel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cat <<READMEBLOCK
+Description
+===========
+
+Requirements
+============
+
+Attributes
+==========
+
+Usage
+=====
+
+Notes
+=====
+
+Tested on:
+
+* Ubuntu lucid
+* Ubuntu precise
+READMEBLOCK
