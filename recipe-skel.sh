924c4961eb3d2b2c30cdcec5e2bd8d9c2b67a252
Thorsten Fischer <thorsten@froschi.org>
2012-06-12 14:39:17 +0200

    recipe-skel.sh: Add ability to handle two arguments.
    To create a recipes/default.rb which correctly handles the lucid/precise
    switch when one or two different package names are provided.
    
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipe-skel.sh
+++ recipe-skel.sh
@@ -1,24 +1,56 @@
 #!/bin/bash
 
-pkgname="$1"
+pkg_lucid=""
+pkg_precise=""
 
-cat <<EOBLOCK
+if [ "$#" -lt "1" ]; then
+  echo "[-] Please provide at least one argument."
+  exit 1
+fi
+
+cat <<HEAD
 packages = Array.new
 
+HEAD
+
+if [ "$#" -eq "1" ]; then
+  pkg_lucid="$1"
+  pkg_precise="$pkg_lucid"
+
+  cat <<DISTBLOCK
 case node[:lsb][:codename]
-when "lucid"
+when "lucid", "precise"
   packages |= %w/
-    $pkgname
-  /
-when "precise"
-  packages |= %w/
-    $pkgname
+    $pkg_lucid
   /
 end
 
+DISTBLOCK
+
+elif [ "$#" -eq "2" ]; then
+  pkg_lucid="$1"
+  pkg_precise="$2"
+
+  cat <<DISTBLOCK
+case node[:lsb][:codename]
+when "lucid"
+  packages |= %w/
+    $pkg_lucid
+  /
+when "precise"
+  packages |= %w/
+    $pkg_precise
+  /
+end
+
+DISTBLOCK
+
+fi
+
+cat <<INSTALLBLOCK
 packages.each do |pkg|
   package pkg do
     action [:install, :upgrade]
   end
 end
-EOBLOCK
+INSTALLBLOCK
